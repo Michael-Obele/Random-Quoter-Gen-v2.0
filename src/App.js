013dee9c109ad237bc79ed334b318ef5a7c5abb8
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-05-20 22:00:03 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/Button';
+import Loading from './componets/Loading/Loading';
+import Footer from './Footer';
+import Stack from 'react-bootstrap/Stack';
+import { FaTwitter, FaClipboard, FaWhatsapp } from 'react-icons/fa';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [freeQuote, setfreeQuote] = useState();
+  const [zenquotes, setZenquotes] = useState();
+  const [count, setcount] = useState(0);
+  const [bcolor, setbcolor] = useState();
+  const [text, setText] = useState();
+
+  useEffect(() => {
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      .then((json) => {
+        setfreeQuote(json);
+        // setLoading(false);
+        // console.log(json);
+      });
+    return setcount(0);
+  }, []);
+  useEffect(() => {
+    fetch(
+      'https://young-gorge-47284.herokuapp.com/https://zenquotes.io/api/quotes/'
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setZenquotes(json);
+        setLoading(false);
+        console.log(json);
+      });
+    return setcount(0);
+  }, [count == 49]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <Card className='justify-content-center quote'>
+          <Card.Body>
+            <Loading />
+          </Card.Body>
+        </Card>
+      ) : (
+        <>
+          <Card className='justify-content-center quote'>
+            <Card.Body>
+              <Stack direction='horizontal' gap={2}>
+                <Card.Title>{freeQuote[count].author}</Card.Title>
+                <div className='bg-light border ms-auto'></div>
+
+                <Button
+                  style={{
+                    backgroundColor: 'green',
+                    color: 'black',
+                    borderColor: 'black',
+                  }}
+                >
+                  <FaTwitter />
+                </Button>
+                <Button variant='secondary'>
+                  <FaClipboard />
+                </Button>
+                <Button variant='secondary'>
+                  <FaWhatsapp />
+                </Button>
+              </Stack>
+              <Card.Text>{freeQuote[count].text}</Card.Text>
+            </Card.Body>
+            <Button
+              id='next-qoute'
+              variant='primary'
+              onClick={() => setcount(count + 1)}
+            >
+              Next Quote
+            </Button>
+          </Card>
+        </>
+      )}
+      {/* <!-- Footer --> */}
+      <Footer />
+      {/* <!-- Footer --> */}
     </div>
   );
 }
