d287514a1447075932672d31dacf3adddf2b0b09
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-05-21 16:09:10 +0100

    More edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
+import { StaticQuotes } from './componets/StaticQuotes/StaticQuotes';
 import React, { useEffect, useState, useRef } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Overlay from 'react-bootstrap/Overlay';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Loading from './componets/Loading/Loading';
 import Footer from './Footer';
-import Stack from 'react-bootstrap/Stack';
-import { FaTwitter, FaClipboard } from 'react-icons/fa';
-import { BsWhatsapp } from 'react-icons/bs';
 
 function App() {
   const bColor = [
@@ -30,12 +23,10 @@ function App() {
   const [randNum, setrandNum] = useState(
     Math.floor(Math.random() * bColor.length) + 1
   );
-  const [show, setShow] = useState();
   const [zenquotes, setZenquotes] = useState();
   const [count, setcount] = useState(0);
-  var tcol = 'black';
   const [bcolor, setbcolor] = useState(bColor[randNum]);
-  const [text, setText] = useState(tcol);
+  const [text, setText] = useState('black');
 
   const nextquote = () => {
     setcount(count + 1);
@@ -43,18 +34,6 @@ function App() {
     setbcolor(bColor[randNum]);
     document.body.style = `background: ${bColor[randNum]}`;
   };
-
-  // tooltip
-  const copyText = () => {
-    navigator.clipboard.writeText(
-      `${zenquotes[count].q} — ${zenquotes[count].a}`
-    );
-    setShow(true);
-    setTimeout(() => {
-      setShow(false);
-    }, 1300);
-  };
-  const target = useRef(null);
 
   // Quotes
   useEffect(() => {
@@ -80,87 +59,19 @@ function App() {
   }, [count === 49]);
   // End of Quotes
   return (
-    <div>
-      {loading ? (
-        <Card className='justify-content-center'>
-          <Card.Body>
-            <Loading />
-          </Card.Body>
-        </Card>
-      ) : (
-        <>
-          <Card className='justify-content-center'>
-            <div className='position-absolute top-0 end-0'>
-              <Button
-                style={{
-                  backgroundColor: bcolor,
-                  color: text,
-                  borderColor: text,
-                }}
-                target='_blank'
-                href={`https://twitter.com/intent/tweet?hashtags=quotes&related=Dev_Obele&text=“${zenquotes[count].q}”—${zenquotes[count].a}`}
-                alt='share to twitter'
-                aria-label='share to twitter'
-              >
-                <FaTwitter />
-              </Button>
-              <Button
-                style={{
-                  backgroundColor: bcolor,
-                  color: text,
-                  borderColor: text,
-                }}
-                alt='copy to clipboard'
-                aria-label='copy to clipboard'
-                ref={target}
-                onClick={copyText}
-              >
-                <FaClipboard />
-              </Button>
-              <Overlay target={target.current} show={show} placement='bottom'>
-                {(props) => (
-                  <Tooltip id='overlay-example' {...props}>
-                    Copied
-                  </Tooltip>
-                )}
-              </Overlay>
-              <Button
-                style={{
-                  backgroundColor: bcolor,
-                  color: text,
-                  borderColor: text,
-                  fontSize: '17px',
-                }}
-                target='_blank'
-                href={`whatsapp://send?text=“${zenquotes[count].q}”—${zenquotes[count].a}`}
-                alt='share to whatsapp'
-                aria-label='send to whatsapp'
-              >
-                <BsWhatsapp />
-              </Button>
-            </div>
-            <Card.Body>
-              <Card.Title id='Author'>{zenquotes[count].a}</Card.Title>
-              <Card.Text id='quote'>{zenquotes[count].q}</Card.Text>
-            </Card.Body>
-            <Button
-              id='next-qoute'
-              style={{
-                backgroundColor: bcolor,
-                color: text,
-                borderColor: text,
-              }}
-              onClick={nextquote}
-            >
-              Next Quote
-            </Button>
-          </Card>
-        </>
-      )}
+    <>
+      <StaticQuotes
+        loading={loading}
+        nextquote={nextquote}
+        zenquotes={zenquotes}
+        count={count}
+        bcolor={bcolor}
+        text={text}
+      />
       {/* <!-- Footer --> */}
       <Footer />
       {/* <!-- Footer --> */}
-    </div>
+    </>
   );
 }
 
---
+++ src/componets/StaticQuotes/StaticQuotes.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './StaticQuotes.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Loading from '../Loading/Loading';
+import { FaTwitter, FaClipboard } from 'react-icons/fa';
+import { BsWhatsapp } from 'react-icons/bs';
+
+export function StaticQuotes({
+  loading,
+  nextquote,
+  zenquotes,
+  count,
+  bcolor,
+  text,
+}) {
+  // tooltip & Copy Text
+  const [show, setShow] = useState();
+  const copyText = () => {
+    navigator.clipboard.writeText(
+      `${zenquotes[count].q} — ${zenquotes[count].a}`
+    );
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 1300);
+  };
+  const target = useRef(null);
+  return (
+    <>
+      {loading ? (
+        <Card className='justify-content-center'>
+          <Card.Body>
+            <Loading />
+          </Card.Body>
+        </Card>
+      ) : (
+        <>
+          <Card className='justify-content-center'>
+            <div className='position-absolute top-0 end-0'>
+              <Button
+                style={{
+                  backgroundColor: bcolor,
+                  color: text,
+                  borderColor: text,
+                }}
+                target='_blank'
+                href={`https://twitter.com/intent/tweet?hashtags=quotes&related=Dev_Obele&text=“${zenquotes[count].q}”—${zenquotes[count].a}`}
+                alt='share to twitter'
+                aria-label='share to twitter'
+              >
+                <FaTwitter />
+              </Button>
+              <Button
+                style={{
+                  backgroundColor: bcolor,
+                  color: text,
+                  borderColor: text,
+                }}
+                alt='copy to clipboard'
+                aria-label='copy to clipboard'
+                ref={target}
+                onClick={copyText}
+              >
+                <FaClipboard />
+              </Button>
+              <Overlay target={target.current} show={show} placement='bottom'>
+                {(props) => (
+                  <Tooltip id='overlay-example' {...props}>
+                    Copied
+                  </Tooltip>
+                )}
+              </Overlay>
+              <Button
+                style={{
+                  backgroundColor: bcolor,
+                  color: text,
+                  borderColor: text,
+                  fontSize: '17px',
+                }}
+                target='_blank'
+                href={`whatsapp://send?text=“${zenquotes[count].q}”—${zenquotes[count].a}`}
+                alt='share to whatsapp'
+                aria-label='send to whatsapp'
+              >
+                <BsWhatsapp />
+              </Button>
+            </div>
+            <Card.Body>
+              <Card.Title id='Author'>{zenquotes[count].a}</Card.Title>
+              <Card.Text id='quote'>{zenquotes[count].q}</Card.Text>
+            </Card.Body>
+            <Button
+              id='next-qoute'
+              style={{
+                backgroundColor: bcolor,
+                color: text,
+                borderColor: text,
+              }}
+              onClick={nextquote}
+            >
+              Next Quote
+            </Button>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
