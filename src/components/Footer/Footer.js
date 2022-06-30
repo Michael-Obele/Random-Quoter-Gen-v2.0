93a3f6d392d874148f2c8ade97847286973eee7d
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-06-30 11:16:00 +0100

    Footer edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,105 +1,52 @@
 import React from 'react';
-import {
-  FaTwitter,
-  FaGoogle,
-  FaInstagram,
-  FaLinkedin,
-  FaGithub,
-  FaWhatsapp,
-} from 'react-icons/fa';
-// import './Footer.css';
+import { FooterItems } from './FooterItems';
 
 export default function Footer({ Darkmode }) {
   const color = Darkmode ? '#202020' : '#f1f1f1'; // if darkmode is true, set background color to black
+  const text = Darkmode ? '#f1f1f1' : '#202020'; // if darkmode is true, set background color to black
+
   return (
+    // <!-- Footer -->
     <footer
-      className='footer mt-auto text-center text-white'
+      class='text-center text-lg-start'
       style={{ backgroundColor: color }}
     >
-      {/* <!-- Twitter --> */}
+      {/* <!-- Section: Social media --> */}
+      <section
+        class='d-flex justify-content-between p-3'
+        style={{ backgroundColor: color }}
+      >
+        {/* <!-- Left --> */}
+        <div class='me-5 m-3'>
+          <span style={{ color: text }}>
+            Get connected with me on social networks:
+          </span>
+        </div>
+        {/* <!-- Left --> */}
 
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='https://twitter.com/Dev_Obele'
-        aria-label='Twitter Link'
-        role='button'
-        bordercolor='black'
-      >
-        <FaTwitter />
-      </a>
-
-      {/* <!-- Google --> */}
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='mailto:amachree9630@gmail.com'
-        role='button'
-        aria-label='Email Link'
-      >
-        <FaGoogle />
-      </a>
-
-      {/* <!-- Instagram --> */}
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='https://www.instagram.com/moa_concepts/'
-        role='button'
-        aria-label='Instagram Link'
-      >
-        <FaInstagram />
-      </a>
-
-      {/* <!-- Linkedin --> */}
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='https://www.linkedin.com/in/michael-amachree-1b71b8210/'
-        role='button'
-        aria-label='Link to my Linkedin profile'
-      >
-        <FaLinkedin />
-      </a>
-      {/* <!-- Github --> */}
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='https://github.com/Michael-Obele'
-        role='button'
-        aria-label='Link to my Github profile'
-      >
-        <FaGithub />
-      </a>
-      {/* <!-- Whatsapp --> */}
-      <a
-        className={
-          Darkmode
-            ? 'btn btn-floating btn-lg  m-1 btn-dark'
-            : 'btn btn-floating btn-lg  m-1 btn-light'
-        }
-        href='https://wa.me/2349069170098?text=Hey,I%20love%20your%20App!'
-        role='button'
-        aria-label='Whatsapp Link'
-      >
-        <i className='fa fa-whatsapp'></i>
-        <FaWhatsapp />
-      </a>
+        {/* <!-- Right --> */}
+        <div className='me-5'>
+          {FooterItems.map((item) => (
+            <a
+              className={
+                Darkmode
+                  ? 'btn btn-floating btn-lg  m-1 btn-dark'
+                  : 'btn btn-floating btn-lg  m-1 btn-light'
+              }
+              target='_blank'
+              style={{ borderColor: 'inherit' }}
+              href={item.href}
+              aria-label={item.aria}
+              role='button'
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+        {/* <!-- Right --> */}
+      </section>
+      {/* <!-- Section: Social media --> */}
     </footer>
+    // <!-- Footer -->
   );
 }
---
+++ src/components/Footer/FooterItems.js
@@ -0,0 +1,54 @@
+import {
+  FaTwitter,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+  FaWhatsapp,
+  FaGlobe,
+} from 'react-icons/fa';
+
+export const FooterItems = [
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/Dev_Obele',
+    icon: <FaTwitter />,
+    aria: 'Twitter Link',
+  },
+  {
+    name: 'Google',
+    href: 'mailto:amachree9630@gmail.com',
+    icon: <FaGoogle />,
+    aria: 'Email Link',
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/moa_concepts/',
+    icon: <FaInstagram />,
+    aria: 'Instagram Link',
+  },
+  {
+    name: 'Linkedin',
+    href: 'https://www.linkedin.com/in/michael-amachree-1b71b8210/',
+    icon: <FaLinkedin />,
+    aria: 'Link to my Linkedin profile',
+  },
+  {
+    name: 'Github',
+    href: 'https://github.com/Michael-Obele',
+    icon: <FaGithub />,
+    aria: 'Github Link',
+  },
+  {
+    name: 'Whatsapp',
+    href: 'https://wa.me/2348160989898',
+    icon: <FaWhatsapp />,
+    aria: 'Whatsapp Link',
+  },
+  {
+    name: 'Website',
+    href: 'https://moaconcept.xyz/',
+    icon: <FaGlobe />,
+    aria: 'Website Link',
+  },
+];
